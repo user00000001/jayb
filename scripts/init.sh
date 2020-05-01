3987b4e44adb1e9130711db510d381e0ddbf5836
暾暾 <631078133@qq.com>
2020-05-01 17:37:52 +0800

    fix inet6 problems.

--- scripts/init.sh
+++ scripts/init.sh
@@ -9,9 +9,9 @@ ssh-keygen -t rsa -P "" -f $HOME/.ssh/id_rsa >/dev/null && cat $HOME/.ssh/id_rsa
 cp -af /etc/ssh ~/; cd ~/ssh; rm -rf ssh_host_* ;for type_key in rsa ecdsa ed25519; do ssh-keygen -t ${type_key} -P "" -f ssh_host_${type_key}_key >/dev/null; done
 curl -s -O https://raw.githubusercontent.com/user00000001/jayb/master/scripts/sshd_config
 (ps -aux|grep "sshd -f sshd_config"|grep -v grep|xargs kill -9) 2>/dev/null
-(ps -aux|grep "ssh -D 2223"|grep -v grep|xargs kill -9) 2>/dev/null
+(ps -aux|grep "ssh -4 -D 2223"|grep -v grep|xargs kill -9) 2>/dev/null
 /usr/sbin/sshd -f sshd_config && sleep 1
-ssh -D 2223 -o StrictHostKeyChecking=no -p 2222 runner@`hostname`
+ssh -4 -D 2223 -o StrictHostKeyChecking=no -p 2222 runner@`hostname`
 curl -s -O https://raw.githubusercontent.com/user00000001/jayb/master/scripts/ngrok
 (ps -aux|grep "ngrok tcp 2223"|grep -v grep|xargs kill -9) 2>/dev/null
 chmod +x ngrok

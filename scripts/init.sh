6b82cd6c02f6cfaaaac300fd9b8a994cddc4baaa
暾暾 <631078133@qq.com>
2020-05-26 09:03:35 +0800

    update scripts.

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,8 +8,8 @@ rm -rf ~/.ssh/*
 ssh-keygen -t rsa -P "" -f $HOME/.ssh/id_rsa >/dev/null && cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys && chmod 600  $HOME/.ssh/authorized_keys
 cp -af /etc/ssh ~/; cd ~/ssh; rm -rf ssh_host_* ;for type_key in rsa ecdsa ed25519; do ssh-keygen -t ${type_key} -P "" -f ssh_host_${type_key}_key >/dev/null; done
 curl -s -O https://raw.githubusercontent.com/user00000001/jayb/master/scripts/sshd_config
-(ps -aux|grep "sshd -f sshd_config"|grep -v grep|xargs kill -9) 2>/dev/null
 (ps -aux|grep "ssh -4 -D 2223"|grep -v grep|xargs kill -9) 2>/dev/null
+(ps -aux|grep "sshd -f sshd_config"|grep -v grep|xargs kill -9) 2>/dev/null
 /usr/sbin/sshd -f sshd_config && sleep 1
 ssh -4 -D 2223 -CfN -o StrictHostKeyChecking=no -p 2222 runner@`hostname` >~/ssh/sshD.log 2>&1 &
 curl -s -O https://raw.githubusercontent.com/user00000001/jayb/master/scripts/sshpass
